3acabb7b21b55e2405e37547ae6c83f9dc6eac91
KaKui <kakuitsang2010@gmail.com>
2021-12-23 22:47:34 +0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by KaKui on 23/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
